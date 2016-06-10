31570e49977387bd548119b81bed6024ee77707a
Josh Welham <josh@aluminati.net>
2016-06-10 12:14:59 +0100

    text input testing

--- tests/autoform.js
+++ tests/autoform.js
@@ -17,6 +17,15 @@ describe('meteor-react-autoform.autoform', () =>
 {
   it('Quick test on all fields exist with basic features like label and floating text', () =>
   {
+    const el = mount(
+      <ReactAutoForm
+        muiTheme={true}
+        onSubmit={onSubmit}
+        schema={HelpDeskSchema}
+        type="insert"
+      />
+    );
+
     expect(el.find('input[name="name"]').props().id).to.contain('name-Yourname-Name-');
     expect(el.find('input[name="nameWithNoFormData"]').props().id).to.contain('nameWithNoFormData-undefined-null-');
     expect(el.find('textarea[name="description"]').props().id).to.contain('description-Irequireapasswordreset-Describeyourproblem-');
@@ -28,17 +37,68 @@ describe('meteor-react-autoform.autoform', () =>
     // expect(el.find('input[name="skyColour"][value="red"]').props().checked).to.be.true;
   });
 
+  it('Should test the text input: name', () =>
+  {
+    const schema = {
+      name: {
+        type: String,
+        materialForm: {
+          floatingLabelText: 'Name',
+          hintText: 'Your name...'
+        }
+      }
+    };
+
+    const el = mount(
+      <ReactAutoForm
+        muiTheme={true}
+        onSubmit={onSubmit}
+        schema={schema}
+        type="insert"
+      />
+    );
+
+    const TextField = el.find('TextField').filterWhere(n => n.props().name === 'name');
+    expect(TextField).length(1);
+    expect(TextField.props().name).to.equal('name');
+    expect(TextField.props().floatingLabelText).to.equal('Name');
+    expect(TextField.props().hintText).to.equal('Your name...');
+    expect(TextField.props().value).to.equal('');
+
+    schema.name.defaultValue = 'My default name';
+    el.setProps({schema});
+    expect(TextField.props().value).to.equal('My default name');
+
+    el.setState({name_fieldValue: 'Josh'});
+    expect(TextField.props().value).to.equal('Josh');
+  });
+
   it('Should test the checkbox radio input: reoccurringProblem', () =>
   {
-    const schema = HelpDeskSchema;
+    const schema = {
+      reoccurringProblem: {
+        type: Boolean,
+        optional: true,
+        label: 'Have you had the problem before?',
+        materialForm: {
+          switcher: 'Checkbox'
+        }
+      }
+    };
+
+    const el = mount(
+      <ReactAutoForm
+        muiTheme={true}
+        onSubmit={onSubmit}
+        schema={schema}
+        type="insert"
+      />
+    );
 
     expect(el.find('Checkbox')).length(1);
     expect(el.find('Checkbox').props().name).to.equal('reoccurringProblem');
     expect(el.find('Checkbox').props().label).to.equal('Have you had the problem before?');
     expect(el.find('Checkbox').props().checked).to.equal(false);
-
-    el.setState({reoccurringProblem_fieldValue: true});
-    expect(el.find('Checkbox').props().checked).to.equal(true);
 
     schema.reoccurringProblem.defaultValue = true;
     el.setProps({schema});
@@ -46,20 +106,38 @@ describe('meteor-react-autoform.autoform', () =>
 
     schema.reoccurringProblem.defaultValue = false;
     el.setProps({schema});
+    expect(el.find('Checkbox').props().checked).to.equal(false);
+
+    el.setState({reoccurringProblem_fieldValue: true});
     expect(el.find('Checkbox').props().checked).to.equal(true);
   });
 
   it('Should test the toggle radio input: agree', () =>
   {
-    const schema = HelpDeskSchema;
+    const schema = {
+      agree: {
+        type: Boolean,
+        optional: true,
+        label: 'Do you agree Toggle?',
+        materialForm: {
+          switcher: 'Toggle'
+        }
+      }
+    };
+
+    const el = mount(
+      <ReactAutoForm
+        muiTheme={true}
+        onSubmit={onSubmit}
+        schema={schema}
+        type="insert"
+      />
+    );
 
     expect(el.find('Toggle')).length(1);
     expect(el.find('Toggle').props().name).to.equal('agree');
     expect(el.find('Toggle').props().label).to.equal('Do you agree Toggle?');
     expect(el.find('Toggle').props().toggled).to.equal(false);
-
-    el.setState({agree_fieldValue: true});
-    expect(el.find('Toggle').props().toggled).to.equal(true);
 
     schema.agree.defaultValue = true;
     el.setProps({schema});
@@ -67,9 +145,14 @@ describe('meteor-react-autoform.autoform', () =>
 
     schema.agree.defaultValue = false;
     el.setProps({schema});
+    expect(el.find('Toggle').props().toggled).to.equal(false);
+
+    el.setState({agree_fieldValue: true});
     expect(el.find('Toggle').props().toggled).to.equal(true);
   });
 });
+
+const onSubmit = sinon.spy();
 
 const HelpDeskSchema = {
   name: {
@@ -187,13 +270,3 @@ const HelpDeskSchema = {
   }
 };
 
-const onSubmit = sinon.spy();
-
-const el = mount(
-  <ReactAutoForm
-    muiTheme={true}
-    onSubmit={onSubmit}
-    schema={HelpDeskSchema}
-    type="insert"
-  />
-);
